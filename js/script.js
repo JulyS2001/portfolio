12258f11a02fdbf0898a67a454f997ee1408d9c9
JulyS <rukysorli@gmail.com>
2026-09-16 16:10:04 -0300

    Add navbar

--- js/script.js
+++ js/script.js
@@ -124,3 +124,12 @@ certificationModal.addEventListener("click", (event) => {
 
 });
 
+// BOTON MENU NAVBAR MOBILE
+
+const navbarToggle = document.getElementById("navbar-toggle");
+const navbarMenu = document.getElementById("navbar-menu");
+
+navbarToggle.addEventListener("click", () => {
+    navbarMenu.classList.toggle("active");
+});
+
